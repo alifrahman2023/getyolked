c6d2de299ed67036a53cc0fe25fed4e36b6214ef
MarcinZarkowski <marcinzarkowski5@gmail.com>
2025-03-08 21:50:01 -0500

    app.py changes

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_jwt_extended import (
-    JWTManager, create_access_token, jwt_required, get_jwt_identity
+    JWTManager, create_access_token, jwt_required, get_jwt_identity, decode_token
 )
 from flask_cors import CORS
 import datetime
