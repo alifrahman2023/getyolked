d289c7945ba9a7704f4896260160a2ed874c1581
MarcinZarkowski <marcinzarkowski5@gmail.com>
2025-03-08 21:24:02 -0500

    changed the navbar

--- frontend/frontend/src/components/NavBar.jsx
+++ frontend/frontend/src/components/NavBar.jsx
@@ -11,15 +11,8 @@ function NavBar({ children }) {
     <div className="WhatHoldsEverything">
       <header className={`header `}>
         <nav className="nav-container">
-          <Link to="/" className="logo">
-            Email Dashboard
-          </Link>
+    
           <ul className="nav-menu">
-            <li className="nav-item">
-              <Link to="/services" className="nav-link">
-                Services
-              </Link>
-            </li>
             <li className="nav-item">
               <a onClick={() => logout()}>Logout</a>
             </li>
